927b3e2e008ba793f51d0a58358b6dd806d8a474
sebastienDOC <s.dechand21@gmail.com>
2024-09-24 17:37:57 +0200

    feat(article-component): add display button and commentary section

--- src/app/article-component/article-component.component.ts
+++ src/app/article-component/article-component.component.ts
@@ -1,14 +1,26 @@
+import { NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-article-component',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, NgStyle],
   templateUrl: './article-component.component.html',
   styleUrls: ['./article-component.component.scss']
 })
 export class ArticleComponentComponent {
-  title: string = 'Tartiflette'
-  imageUrl: string = "https://i.ibb.co/JK3dvBD/tartiflette.jpg"
-  description: string = "La tartiflette est une recette de cuisine à base de gratin de pommes de terre, d'oignons, de lardons et de crème fraîche, le tout gratiné au reblochon"
+  article = {
+    title: 'Tartiflette',
+    author: 'Seb D.',
+    content: "La tartiflette est une recette de cuisine à base de gratin de pommes de terre, d'oignons, de lardons et de crème fraîche, le tout gratiné au reblochon",
+    image: 'https://i.ibb.co/JK3dvBD/tartiflette.jpg',
+    isPublished: true,
+    comment: ''
+  };
+
+  togglePublication(): void {
+    this.article.isPublished = !this.article.isPublished;
+  }
+
 }
