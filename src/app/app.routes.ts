d0150df805d9f593913d87f13b905a6b2fb4367e
sebastienDOC <s.dechand21@gmail.com>
2024-09-30 16:39:21 +0200

    feat(): add form and error messages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { ArticlePageComponent } from './article-page/article-page.component';
 import { ArticlesListComponent } from './articles-list/articles-list.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
 
 export const routes: Routes = [
   {path: '', component: LandingPageComponent},
   {path: 'articles', component: ArticlesListComponent},
   {path: 'article/:id', component: ArticlePageComponent},
+  {path: 'contact', component: ContactFormComponent},
   {path: '**', component: NotFoundComponent}
 ];
---
+++ src/app/contact-form/contact-form.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { Message } from '../../models/message.model';
+import { FooterComponent } from "../footer/footer.component";
+import { HeaderComponent } from "../header/header.component";
+
+@Component({
+  selector: 'app-contact-form',
+  standalone: true,
+  imports: [FormsModule, FooterComponent, HeaderComponent],
+  templateUrl: './contact-form.component.html',
+  styleUrl: './contact-form.component.scss'
+})
+export class ContactFormComponent {
+  isSubmitted: boolean = false
+
+  newMessage: Message = {
+    lastname: "",
+    firstname: "",
+    email: "",
+    content: ""
+  };
+
+  onSubmit(contactForm: NgForm): void {
+    this.isSubmitted = true
+    if (contactForm.valid) {
+      console.log('Formulaire envoyé !', this.newMessage);
+      contactForm.reset()
+      this.isSubmitted = false
+    } else {
+      console.log('Formulaire invalide');
+    }
+  }
+
+}
